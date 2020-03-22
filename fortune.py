fd420048869da25e6c920fd401c12909e0318251
Sanjoy Ganguli <sanjoyganguli29@gmail.com>
2020-03-22 14:15:01 -0400

    Update fortune.py

--- fortune.py
+++ fortune.py
@@ -4,5 +4,5 @@ def fortune_login():
 driver.get ("https://fortune.com/")
 
 driver.find_element_by_id("email").send_keys('sanjoyganguli@hotmail.com')
-driver.find_element_by_id("pass").send_keys("sg99sg01!$")
+driver.find_element_by_id("pass").send_keys("fakepassword")
 driver.find_element_by_id("loginbutton").click
